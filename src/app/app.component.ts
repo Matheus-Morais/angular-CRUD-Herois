19bce9f27a9c6a54b67628658280c251800ea48d
ronicley silva de sa <ronicley silva de sa>
2017-09-16 16:43:09 -0300

    Alteraçoes gerais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,16 @@ export class AppComponent implements OnInit {
     title: string;
     vingadores: Array<Vingador>;
     selecionado: Vingador;
-    novo: Vingador = new Vingador(0, '', '');
+    novo: Vingador = new Vingador(0, '', '','');
 
     constructor() {
         this.title = 'Vingadores';
         this.vingadores = [
-            new Vingador(1, 'Capitão América', 'Steve Rogers'),
-            new Vingador(2, 'Viúva Negra', 'Natasha Romanoff'),
-            new Vingador(3, 'Ms. Marvel', 'Carol Danvers'),
-            new Vingador(4, 'Deadpool', 'Wade Wilson'),
-            new Vingador(5, 'Gavião Arqueiro', 'Clint Barton')
+            new Vingador(0, 'Capitão América', 'Steve Rogers',  'https://upload.wikimedia.org/wikipedia/pt/b/bc/Steven_Rogers_%28Earth-616%29.jpg'),
+            new Vingador(1, 'Viúva Negra', 'Natasha Romanoff', 'https://upload.wikimedia.org/wikipedia/pt/1/1c/Black_Widow_Vol_6_12.jpg'),
+            new Vingador(2, 'Ms. Marvel', 'Carol Danvers', 'https://i.pinimg.com/736x/b1/f0/a1/b1f0a1bc45eab780722ebc34b3145e25--captain-marvel-shazam-marvel-dc.jpg'),
+            new Vingador(3, 'Deadpool', 'Wade Wilson', 'https://upload.wikimedia.org/wikipedia/pt/c/ce/Deadpool_Vol_4_7.jpg'),
+            new Vingador(4, 'Gavião Arqueiro', 'Clint Barton', 'https://upload.wikimedia.org/wikipedia/pt/c/cc/Gavi%C3%A3o_Arqueiro_por_Clint_Langley.JPG')
         ];
     }
 
@@ -33,22 +33,24 @@ export class AppComponent implements OnInit {
 
     cadastrar(): void {
         const novoId: number = this.vingadores.length + 1;       
-        this.vingadores.push(new Vingador(novoId, this.novo.nome, this.novo.pessoa));
-        this.novo = new Vingador(0, '', '');
+        this.vingadores.push(new Vingador(novoId, this.novo.nome, this.novo.pessoa, this.novo.urlDaFoto));
+        this.novo = new Vingador(0, '', '','');
     }
 
-    RemoverVingador(idVingador: number): void{ //idVingador é o id que o usuario passa
-
-        let idv: number = idVingador - 1; //Como o id que ele passou é diferente da posição dele no array, é feito isso
-        console.log(this.vingadores[idv]);
-        if(this.vingadores[idv].id == idVingador) //Só exclui se o id for igual ao id que o usuario passou, se o usuario passar um id que nao existe não vai ser excluido
-        {
-            this.vingadores.splice(idv, 1);
+    RemoverVingador(index: number): void{ //nome é o nome do Vingador passado por parametro
+            //this.vingadores.splice(nome, 1);
+        var id = index;
+        for (let i of this.vingadores) {
+            if(index== i.id){
+                if(this.vingadores.length!=1){
+                    this.vingadores.splice(id,1);
+                
+                }
+                else{
+                    this.vingadores.splice(0,1)
+                }
+                
+            }
         }
-        console.log(this.vingadores)
-    }
-
-    RemoverV(selecionado: number): void{
-        this.vingadores.splice(selecionado, 1);
     }
 }
--- src/app/vingador.ts
+++ src/app/vingador.ts
@@ -2,10 +2,12 @@ export class Vingador {
     id: number;
     nome: string;
     pessoa: string;
+    urlDaFoto: string;
 
-    constructor(id: number, nome: string, pessoa?: string) {
+    constructor(id: number, nome: string, pessoa: string,  urlDaFoto: string) {
         this.id = id;
         this.nome = nome;
         this.pessoa = pessoa;
+        this.urlDaFoto=urlDaFoto;
     }
 }
